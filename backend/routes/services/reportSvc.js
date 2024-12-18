7056696ccc397ad9ff83efbe2495e3896b839790
DAVE-PC\davel <davelaurence.santillan08@gmail.com>
2024-12-18 12:48:51 +0800

    sync pull

--- backend/routes/services/reportSvc.js
+++
@@ -1,8 +0,0 @@
-const ReportsController = require('../../controller/_report/Reports');
-
-const get = [
-    [ '/print-summary-by-cat', ReportsController.PrintSummaryByCat ],
-    [ '/print-summary-by-date-range', ReportsController.PrintSummaryByDateRange ]
-]
-
-module.exports.routes = { get: get }
