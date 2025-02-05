41b60b0172367d1a20d9654a7aac4fdea5b906e0
DESKTOP-VLTCHR6\Roy Developer <rjisraelcubillan55@gmail.com>
2025-02-06 02:14:09 +0800

    added: added disapprove dialog/ input security/ view data done

---
+++ frontend-dmpi/src/views/Administrative/ForConfirmation/components/input-security-dialog.jsx
@@ -0,0 +1,39 @@
+import { useDispatch, useSelector } from "react-redux";
+import { OPEN_CONFIRM } from "../../../../store/actions";
+import CustomDialog from "../../../../components/CustomDialog";
+import { Box, Button, Grid, TextField } from "@mui/material";
+
+
+const InputSecurityDialog = () => {
+  const dispatch = useDispatch();
+  const open = useSelector((state) => state.customization.openConfirm);
+  const CloseDialog = () => { dispatch({ type: OPEN_CONFIRM, openConfirm: false }); }
+  
+  return (
+    <CustomDialog 
+        open={open}
+        onClose={CloseDialog}
+        maxWidth={'sm'}
+        DialogTitles={"Input Security Key to Confirm"}
+        DialogContents={
+            <Box sx={{ padding: 0 }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} sm={12}>
+                        <TextField fullWidth size="small" />
+                    </Grid>
+                </Grid>
+            </Box>
+        }
+        DialogAction={
+            <Grid container spacing={1}>
+                <Grid item xs={12} sm={12} sx={{ display: 'flex', justifyContent: 'flex-end', gap: 1 }}>
+                    <Button variant="contained" color="primary" size="large">Confirm</Button>
+                    <Button variant="contained" color="error" size="large">Cancel</Button>
+                </Grid>
+            </Grid>
+        }
+    />
+  )
+}
+
+export default InputSecurityDialog
---
+++ frontend-dmpi/src/views/Administrative/ForConfirmation/components/view-data-dialog.jsx
@@ -0,0 +1,52 @@
+import { useDispatch, useSelector } from "react-redux";
+import { OPEN_CONFIRM, OPEN_CUSTOM_MODAL, OPEN_DISAPPROVE } from "../../../../store/actions";
+import CustomDialog from "../../../../components/CustomDialog";
+import { Box, Button, Grid } from "@mui/material";
+import Iframe from "react-iframe";
+import { Fragment } from "react";
+import InputSecurityDialog from "./input-security-dialog";
+import DisapproveDialog from "./disapprove-dialog";
+
+
+const ViewDataDialog = () => {
+  const dispatch = useDispatch();
+  const open = useSelector((state) => state.customization.openCustomModal);
+  const CloseDialog = () => { dispatch({ type: OPEN_CUSTOM_MODAL, openCustomModal: false }); }
+  const openConfirm = () => { dispatch({ type: OPEN_CONFIRM, openConfirm: true }); }
+  const openDisapprove = () => { dispatch({ type: OPEN_DISAPPROVE, openDisapprove: true }); }
+  return (
+    <Fragment>
+    <InputSecurityDialog />
+    <DisapproveDialog />
+    <CustomDialog 
+        fullScreen={true}
+        open={open}
+        onClose={CloseDialog}
+        DialogContents={
+            <Box sx={{ padding: 0 }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6}>
+                        DAR Preview
+                        <Iframe width="100%" height="700px" alt='not found' ></Iframe>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        SOA Preview
+                        <Iframe width="100%" height="700px" alt='not found' ></Iframe>
+                    </Grid>
+                </Grid>
+            </Box>
+        }
+        DialogAction={
+            <Grid container spacing={1}>
+                <Grid item xs={12} sm={12} sx={{ display: 'flex', justifyContent: 'center', gap: 3 }}>
+                <Button variant="contained" color="primary" size="large" onClick={openConfirm}>Confirm</Button>
+                    <Button variant="contained" color="error" size="large" onClick={openDisapprove} >Disapprove</Button>
+                </Grid>
+            </Grid>
+        }
+    />
+    </Fragment>
+  )
+}
+
+export default ViewDataDialog
